fd948f6c9fd3a0e5d6769a02fac15a6289c0ba88
Jean Gérard Bousiquot <jgb@JGB-MacBook-Pro.local>
2016-06-21 14:07:06 -0400

    improving the display of single.php

--- single.php
+++ single.php
@@ -11,7 +11,14 @@
 		<h2 class="htitle txt30  txt25m txtcenterm text-center"><?= get_the_title(); ?></h2>
 		<p class="post-meta text-center">
 			<em>
-				Posté le <?php the_time('d/m/Y \à g:i A'); ?>
+				Posté le <?php the_time('d/m/Y \à g:i A'); ?> dans
+				<?php
+				$category = get_the_category();
+				if ($category) {
+					echo '<a href="' . get_category_link( $category[0]->term_id ) .
+						'" title="' . sprintf( __( "Voir tous articles dans la catégorie %s" ),
+							$category[0]->name ) . '" ' . '>' . $category[0]->name.'</a> ';
+				}?>
 				par <a href="<?= home_url('/' ); ?>">la rédaction</a>.
 			</em>
 		</p>
@@ -21,7 +28,16 @@
 
 		<div class="linemam w100 ">
 
-			<article><?php the_content(); ?></article>
+			<article>
+				<?php the_content(); ?>
+				
+				<br>
+				<br>
+
+				<p>
+					<?php the_tags( 'Mots-clés: ', ', ', '' ); ?>
+				</p>
+			</article>
 
 		</div>
 
